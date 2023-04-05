3cfa015f1366b5cd78a818f1d1144f25a69716c2
PrinceCharles47 <clemente.pcharles@gmail.com>
2023-04-05 15:32:43 +0700

    add navigation (partial)

---
+++ src/components/Navigation.vue
@@ -0,0 +1,30 @@
+<template>
+    <v-app-bar app dense flat id="navbar">
+        <p class="font-italic white--text pt-4">Ani<span class="font-weight-bold">TRACE</span></p>
+
+        <div class="ml-auto">
+            <v-btn text class="white--text">Sign In</v-btn>
+            <v-btn text class="white--text">Dashboard</v-btn>
+            <v-btn text class="white--text">Back</v-btn>
+            <v-btn text class="white--text">Log Out</v-btn>
+        </div>
+    </v-app-bar>
+</template>
+  
+<script>
+
+export default {
+    name: 'Navigation',
+
+    data: () => ({
+        //
+    }),
+};
+</script>
+  
+<style>
+#navbar {
+    background: transparent;
+}
+</style>
+  
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,15 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <landing-page />
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+  import LandingPage from '../components/LandingPage'
 
-export default {
-  name: 'HomeView',
-  components: {
-    HelloWorld
+  export default {
+    name: 'Home',
+
+    components: {
+      LandingPage,
+    },
   }
-}
 </script>
