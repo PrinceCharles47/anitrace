b2119ebfcff4c035aa38de97e2e80ec98fa3927d
PrinceCharles47 <clemente.pcharles@gmail.com>
2023-04-08 19:52:40 +0700

    add navigation

--- src/components/Navigation.vue
+++ src/components/Navigation.vue
@@ -1,24 +1,57 @@
 <template>
-    <v-app-bar app dense flat id="navbar">
-        <p class="font-italic white--text pt-4">Ani<span class="font-weight-bold">TRACE</span></p>
+    <v-app-bar dense flat id="navbar">
+        <p class="font-italic white--text pt-4 ml-4">Ani<span class="font-weight-bold">TRACE</span></p>
 
         <div class="ml-auto">
-            <v-btn text class="white--text">Sign In</v-btn>
-            <v-btn text class="white--text">Dashboard</v-btn>
-            <v-btn text class="white--text">Back</v-btn>
-            <v-btn text class="white--text">Log Out</v-btn>
+            <v-btn plain class="white--text" v-if="isHomepage" to="/signin">Sign In</v-btn>
+            <!-- <v-btn plain class="white--text">Dashboard</v-btn> -->
+            <v-btn plain class="white--text" @click="signOut()" v-if="isLoggedIn">Log Out</v-btn>
+            <v-btn plain class="white--text" to="/">Back</v-btn>
         </div>
     </v-app-bar>
 </template>
   
 <script>
+import firebase from 'firebase/compat/app'
+import 'firebase/compat/auth'
 
 export default {
     name: 'Navigation',
 
     data: () => ({
-        //
+        isLoggedIn: false,
+        isHomepage: true
     }),
+    methods: {
+
+        signOut: function () {
+            firebase.auth().signOut().then(() => {
+                this.$router.push('signin')
+            })
+        }
+
+    },
+    watch: {
+
+        '$route.name' : function(newVal, oldVal) {
+            console.log(newVal);
+            if(newVal === 'upload'){
+
+                this.isLoggedIn = true
+                this.isHomepage = false
+
+            }else if(newVal === 'home'){
+
+                this.isLoggedIn = false
+                this.isHomepage = true
+
+            }else{
+                this.isLoggedIn = false
+                this.isHomepage = false
+            }
+        }
+
+    }
 };
 </script>
   
